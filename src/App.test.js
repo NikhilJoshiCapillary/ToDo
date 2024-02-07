4f98d0de498814f426aa25c28e3f2d3f831e26e7
Nikhil_Joshi <nikhil.joshi@capillary.com>
2024-02-07 12:11:48 +0530

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -15,7 +15,140 @@ function initialProps(){
   render(<Provider store={store}><Todos/></Provider>);
 }
 
-test.todo("add all the test")
 
-// looking for hotfix
+describe("All about home page", ()=>{
+  test(("check for all ui-elements"), ()=>{
+    initialProps();
+    const getInputBar = screen.getByTestId("input-bar")
+    const addingBtn = screen.getByTestId("add-btn")
+    expect(getInputBar).toBeInTheDocument()
+    expect(addingBtn).toBeInTheDocument()
+  })
+
+  test(("check for all ui-elements-2"), ()=>{
+    initialProps();
+    const getInputBar = screen.getByTestId("input-bar")
+    const addingBtn = screen.getByTestId("add-btn")
+    expect(getInputBar).toBeInTheDocument()
+    expect(addingBtn).toBeInTheDocument()
+  })
+
+
+
+  test("testing addition of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    expect(screen.getByText("Go to Park")).toBeInTheDocument();
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn)
+  })
+
+
+  test("testing removal of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    const text = screen.queryByText("Go to Park");
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn);
+    expect(text).not.toBeInTheDocument()
+  })
+
+
+  test("testing editing  of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    const text = screen.queryByText("Go to Park");
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn);
+    expect(text).not.toBeInTheDocument()
+  })
+
+})
+
+
+describe("Testing navbar", ()=>{
+  test("check for navbar ui-elements", () => {
+    render(
+      
+        <BrowserRouter>
+          <Navbar />
+        </BrowserRouter>
+  
+    );
+    const homeButton = screen.getByTestId("home-btn");
+    const aboutButton = screen.getByTestId("about-btn");
+    const contactButton = screen.getByTestId("contact-btn");
+    const getHeading = screen.getByText("Zen-Task Management App")
+    expect(homeButton).toBeInTheDocument();
+    expect(aboutButton).toBeInTheDocument();
+    expect(contactButton).toBeInTheDocument();
+    expect(getHeading).toBeInTheDocument();
+  });
+
+
+  test("Testing add-task reducer", ()=>{
+    const initial_state = {
+      array: [],
+    };
+    const action = {type:"CREATE_TASK", payload:"Go to the mall"}
+    const new_state = globalReducer(initial_state, action).array
+    expect(new_state).toHaveLength(1);
+  })
+
+  test("Testing remove-task reducers", ()=>{
+    const initial_state = {
+      array: ["Go to the mall"]
+    };
+    const action = {type:"REMOVE_TASK", payload:0}
+    const new_state = globalReducer(initial_state, action).array
+    expect(new_state).toHaveLength(0);
+  })
+
+
+  test("Testing random-type reducers", ()=>{
+    const initial_state = {
+      array: []
+    };
+    const action = {type:"RANDOM"}
+    const new_state = globalReducer(initial_state, action).array
+    expect(new_state).toHaveLength(0);
+  })
+
+
+
+})
+
+
+
+
+describe("Looking for the edit button", ()=>{
+  
+  test("checking for the edit button", ()=>{
+    initialProps()
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    const editBtn = screen.getByTestId("edit-btn")
+    expect(editBtn).toBeInTheDocument()
+  })
+})
+
 
